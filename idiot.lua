831c2ad727ee5b749615d3097f981adde7264d42
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-04-06 15:03:26 +0700

    Update idiot.lua

--- idiot.lua
+++ idiot.lua
@@ -262,6 +262,46 @@ return IsPlayerWhitelisted
 	end    
 })
 
+SettingsTab:AddButton({
+	Name = "Crash server (Need btools)",
+	Callback = function()
+        local runService = game:GetService("RunService")
+local localPlayer = game:GetService("Players").LocalPlayer
+local camera = workspace.CurrentCamera
+local tick = 0
+local tbl = {}
+local max = 2500
+
+local crashPercent = Drawing.new("Text")
+crashPercent.Outline = true
+crashPercent.Color = Color3.fromRGB(255, 255, 255)
+crashPercent.Center = true
+crashPercent.Visible = true
+crashPercent.Transparency = 1
+crashPercent.Font = Drawing.Fonts.Plex
+crashPercent.Size = 100
+
+runService.Heartbeat:Connect(function()
+    if tick > max then
+        for i, v in pairs(workspace:GetChildren()) do
+            if v:IsA("Part") then
+                tbl[#tbl + 1] = {
+                    ["Part"] = v,
+                    ["Anchored"] = false
+                }
+            end
+        end
+        localPlayer.Character["Building Tools"].SyncAPI.ServerEndpoint:InvokeServer("SyncAnchor", tbl)
+    else
+        localPlayer.Character["Building Tools"].SyncAPI.ServerEndpoint:InvokeServer("CreatePart", "Normal", CFrame.new(-3, 50, -70), workspace)
+        crashPercent.Position = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
+        tick += 1
+        crashPercent.Text = "Crash Ban " .. math.floor(tick / max * 100) .. "%"
+    end
+end)
+  	end    
+})
+
 local SettingsTab = Window:MakeTab({
 	Name = "Settings",
 	Icon = "rbxassetid://4483345998",
