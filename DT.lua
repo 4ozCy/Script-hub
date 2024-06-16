8da717483ff64a768c2202d62b2439cb284e0749
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-06-17 01:55:45 +0700

    Update and rename Nerd.lua to DT.lua

--- DT.lua
+++ DT.lua
@@ -1,14 +1,14 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 OrionLib:MakeNotification({
-	Name = "Welcome to Nerd hub",
+	Name = "Welcome to DT hub",
 	Content = "You're a Nerd",
 	Image = "rbxassetid://4483345998",
 	Time = 8
 })
 
 
-local Window = OrionLib:MakeWindow({Name = "Nerd hub", HidePremium = false, SaveConfig = false, ConfigFolder = "Orion"})
+local Window = OrionLib:MakeWindow({Name = "DT hub", HidePremium = false, SaveConfig = false, ConfigFolder = "Orion"})
 
 --Player Tab--
 
@@ -336,7 +336,7 @@ end)
 })
 
 MainTab:AddButton({
-	Name = "Nerd hub aimlock",
+	Name = "DT hub aimlock",
 	Callback = function()
         -- Gui to Lua
 -- Version: 3.2
@@ -372,7 +372,7 @@ TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
 TextLabel.Size = UDim2.new(1.00234735, 0, 1.08253634, 0)
 TextLabel.Font = Enum.Font.SourceSansSemibold
-TextLabel.Text = "Nerd Hub | Aimlock"
+TextLabel.Text = "DT Hub | Aimlock"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 16.000
 
@@ -553,7 +553,7 @@ for _, playerName in ipairs(getAllPlayers()) do
 })
 
 MainTab:AddToggle({
-	Name = "Touch fling",
+	Name = "Touch fling (might not work sometime)",
 	Default = false,
 	Callback = function(Value)
 		if Value then
@@ -614,7 +614,6 @@ end
 local function serverHop()
     local servers = getServerList()
     if #servers > 0 then
-        -- Attempt to join the server with the smallest number of players
         TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[1].id, LocalPlayer)
     else
         warn("No available servers to hop to.")
@@ -642,7 +641,7 @@ local OtherTab = Window:MakeTab({
 OtherTab:AddButton({
 	Name = "Teleport tools",
 	Callback = function()
-	getgenv().speed = 333 -- Change this to control the speed of the tool
+	getgenv().speed = 500
  
 -- Script
 local Players = game:GetService("Players")
@@ -676,6 +675,91 @@ tool.Parent = LocalPlayer.Backpack
   	end    
 })
 
+otherTab:AddButton({
+        Name = "anti-fling",
+	Callback = function()
+local Services = setmetatable({}, {__index = function(Self, Index)
+local NewService = game.GetService(game, Index)
+if NewService then
+Self[Index] = NewService
+end
+return NewService
+end})
+
+local LocalPlayer = Services.Players.LocalPlayer
+
+local function PlayerAdded(Player)
+   local Detected = false
+   local Character;
+   local PrimaryPart;
+
+   local function CharacterAdded(NewCharacter)
+       Character = NewCharacter
+       repeat
+           wait()
+           PrimaryPart = NewCharacter:FindFirstChild("HumanoidRootPart")
+       until PrimaryPart
+       Detected = false
+   end
+
+   CharacterAdded(Player.Character or Player.CharacterAdded:Wait())
+   Player.CharacterAdded:Connect(CharacterAdded)
+   Services.RunService.Heartbeat:Connect(function()
+       if (Character and Character:IsDescendantOf(workspace)) and (PrimaryPart and PrimaryPart:IsDescendantOf(Character)) then
+           if PrimaryPart.AssemblyAngularVelocity.Magnitude > 50 or PrimaryPart.AssemblyLinearVelocity.Magnitude > 100 then
+               if Detected == false then
+                   game.StarterGui:SetCore("ChatMakeSystemMessage", {
+                       Text = "Fling Exploit detected, Player: " .. tostring(Player);
+                       Color = Color3.fromRGB(255, 200, 0);
+                   })
+               end
+               Detected = true
+               for i,v in ipairs(Character:GetDescendants()) do
+                   if v:IsA("BasePart") then
+                       v.CanCollide = false
+                       v.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+                       v.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+                       v.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
+                   end
+               end
+               PrimaryPart.CanCollide = false
+               PrimaryPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+               PrimaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+               PrimaryPart.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
+           end
+       end
+   end)
+end
+
+-- // Event Listeners \\ --
+for i,v in ipairs(Services.Players:GetPlayers()) do
+   if v ~= LocalPlayer then
+       PlayerAdded(v)
+   end
+end
+Services.Players.PlayerAdded:Connect(PlayerAdded)
+
+local LastPosition = nil
+Services.RunService.Heartbeat:Connect(function()
+   pcall(function()
+       local PrimaryPart = LocalPlayer.Character.PrimaryPart
+       if PrimaryPart.AssemblyLinearVelocity.Magnitude > 250 or PrimaryPart.AssemblyAngularVelocity.Magnitude > 250 then
+           PrimaryPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+           PrimaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+           PrimaryPart.CFrame = LastPosition
+
+           game.StarterGui:SetCore("ChatMakeSystemMessage", {
+               Text = "You were flung. Neutralizing velocity.";
+               Color = Color3.fromRGB(255, 0, 0);
+           })
+       elseif PrimaryPart.AssemblyLinearVelocity.Magnitude < 50 or PrimaryPart.AssemblyAngularVelocity.Magnitude > 50 then
+           LastPosition = PrimaryPart.CFrame
+       end
+   end)
+end)
+	end
+})
+		
 local SettingsTab = Window:MakeTab({
 	Name = "[⚙️] Settings",
 	Icon = "rbxassetid://4483345998",
