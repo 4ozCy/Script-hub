ae27ac13ac4b7814a1915f2fc39b2e76c6716dbf
Zcy. <157526994+Zyneeee@users.noreply.github.com>
2024-07-31 20:59:39 +0700

    Update New.lua

--- New.lua
+++ New.lua
@@ -5,5 +5,12 @@ local mainWindow = Library:NewWindow("Skidd hub")
 local main = mainWindow:NewSection("Main")
 
 main:CreateButton("teleport", function()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(287.6754455566406, 439.5106201171875, 480.32952880859375)
-end)
+local player = game.Players.LocalPlayer
+local humanoidRootPart = player.Character.HumanoidRootPart
+
+local teleportPosition1 = CFrame.new(287.6754, 439.5106, 480.3295)
+local teleportPosition2 = CFrame.new(239.7410, 443.8971, -110.4596)
+
+humanoidRootPart.CFrame = teleportPosition1
+wait(3.2)
+humanoidRootPart.CFrame = teleportPosition2
