efaa55b91e68a9191bf7e001b9bc2808479eb42a
Zcy. <157526994+Zyneeee@users.noreply.github.com>
2024-07-31 15:14:09 +0700

    Update New.lua

--- New.lua
+++ New.lua
@@ -2,6 +2,8 @@ local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/blood
 
 local mainWindow = Library:NewWindow("Skidd hub")
 
+local main = mainWindow:NewSection("Main")
+
 main:CreateButton("teleport", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(287.6754455566406, 439.5106201171875, 480.32952880859375)
 end)
