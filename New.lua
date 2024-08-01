1a1618d1e88409a9ef880ebd71251d8ed300f96e
Zcy. <157526994+Zyneeee@users.noreply.github.com>
2024-08-01 14:24:59 +0700

    Update New.lua

--- New.lua
+++ New.lua
@@ -9,10 +9,10 @@ local player = game.Players.LocalPlayer
 local humanoidRootPart = player.Character.HumanoidRootPart
 
 local teleportPosition1 = CFrame.new(287.6754, 439.5106, 480.3295)
-local teleportPosition2 = CFrame.new(239.7410, 443.8971, -110.4596)
+local teleportPosition2 = CFrame.new(-22.259986877441406, 472.40972900390625, 113.41948699951172)
 
 humanoidRootPart.CFrame = teleportPosition1
-wait(3.2)
+wait(3)
 humanoidRootPart.CFrame = teleportPosition2
     
 end)
