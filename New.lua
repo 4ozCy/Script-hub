65c0f42c27007f782641516a1ad9bac3554a2d3e
Zcy. <157526994+Zyneeee@users.noreply.github.com>
2024-08-01 12:49:50 +0700

    Update New.lua

--- New.lua
+++ New.lua
@@ -20,17 +20,25 @@ end)
 local main = mainWindow:NewSection("Player")
 
 main:CreateTextbox("Username", function(text)
-    local targetUsername = text
+    targetUsername = text
 end)
 
 main:CreateButton("teleport", function()
-    local player = game.Players.LocalPlayer
-    local humanoidRootPart = player.Character.HumanoidRootPart
-
-    local targetPlayer = game.Players:FindFirstChild(targetUsername)
+    local players = getPlayer(targetUsername, game.Players.LocalPlayer)
     
-    if targetPlayer and targetPlayer.Character then
-        local targetHumanoidRootPart = targetPlayer.Character.HumanoidRootPart
-        humanoidRootPart.CFrame = targetHumanoidRootPart.CFrame
+    for i, v in pairs(players) do
+        if game.Players[v] and game.Players[v].Character then
+            local speaker = game.Players.LocalPlayer
+            local humanoid = speaker.Character:FindFirstChildOfClass('Humanoid')
+
+            if humanoid and humanoid.SeatPart then
+                humanoid.Sit = false
+                wait(0.1)
+            end
+            
+            local speakerRoot = getRoot(speaker.Character)
+            local targetRoot = getRoot(game.Players[v].Character)
+            speakerRoot.CFrame = targetRoot.CFrame + Vector3.new(3, 1, 0)
+        end
     end
 end)
