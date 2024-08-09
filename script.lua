62f9f7a81e8a3484af4b31472c741105a51ab869
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-08-10 02:12:34 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,7 +2,9 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/blood
 
 local main = library:Window("nozcy's hub")
 
-main:Button("small place", function()
+main:Label("Main" Color3.fromRGB(127, 143, 166))
+
+main:Button("Small Place", function()
 local player = game.Players.LocalPlayer
 local humanoidRootPart = player.Character.HumanoidRootPart
 
@@ -14,7 +16,16 @@ wait(2.3)
 humanoidRootPart.CFrame = teleportPosition2
 end)
 
-main:Button("Void", function()
+main:Button("Mountain", function()
+local teleportPosition = Vector3.new(360.623, 699.103, 374.761)
+
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local rootPart = character:WaitForChild("HumanoidRootPart")
+
+rootPart.CFrame = CFrame.new(teleportPosition)
+end)
+main:Button("Under Map", function()
 local player = game.Players.LocalPlayer
 local humanoidRootPart = player.Character.HumanoidRootPart
 
@@ -24,3 +35,56 @@ humanoidRootPart.CFrame = teleportPosition1
 wait(1.1)
 humanoidRootPart.CFrame = teleportPosition2
 end)
+
+main:Button("Basement", function()
+local teleportPosition = Vector3.new(567.5752563476562, 439.5106201171875, -414.8644714355469)
+
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local rootPart = character:WaitForChild("HumanoidRootPart")
+
+rootPart.CFrame = CFrame.new(teleportPosition)
+end)
+
+local other = library:Window("nozcy's hub")
+
+other:Label("Misc", Color3.fromRGB(127, 143, 166))
+
+other:Box("Username", function(text)
+targetUsername = text
+end)
+
+misc:Button("teleport", function()
+local function getPlayer(username, speaker)
+        local foundPlayers = {}
+        for _, player in pairs(game.Players:GetPlayers()) do
+            if player.Name:lower():sub(1, #username) == username:lower() then
+                table.insert(foundPlayers, player.Name)
+            end
+        end
+        return foundPlayers
+    end
+
+    local function getRoot(character)
+        return character:FindFirstChild("HumanoidRootPart") or character:FindFirstChild("Torso") or character:FindFirstChild("UpperTorso")
+    end
+
+    local players = getPlayer(targetUsername, game.Players.LocalPlayer)
+    
+    for i, v in pairs(players) do
+        if game.Players[v] and game.Players[v].Character then
+            local speaker = game.Players.LocalPlayer
+            local humanoid = speaker.Character:FindFirstChildOfClass('Humanoid')
+
+            if humanoid and humanoid.SeatPart then
+                humanoid.Sit = false
+                wait(0.1)
+            end
+            
+            local speakerRoot = getRoot(speaker.Character)
+            local targetRoot = getRoot(game.Players[v].Character)
+            speakerRoot.CFrame = targetRoot.CFrame + Vector3.new(1, 1, 1)
+        end
+    end
+end)
+  
